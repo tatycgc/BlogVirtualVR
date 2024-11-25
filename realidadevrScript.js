b626050afc9714e88fb21e95ee7d536fa2ceba07
tatycgc <tatyanacgc5@gmail.com>
2024-11-25 04:46:11 -0300

    Update realidadevrScript.js

--- realidadevrScript.js
+++ realidadevrScript.js
@@ -1,3 +1,13 @@
+function hamburg() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(0px)";
+}
+
+function cancel() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(-500px)";
+}
+
 const slides = document.querySelectorAll('.slider .slide img');
 
 function move3DEffect(event) {
